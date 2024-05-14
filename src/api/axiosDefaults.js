34cf8b18213cf8bf30b1f09f1b4c06a3213036ae
Ruairidh MacArthur <roomacarthur@gmail.com>
2024-05-14 13:34:55 +0000

    Add: API, axios defaults

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://drfapi-roo-f5011a5fe545.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
